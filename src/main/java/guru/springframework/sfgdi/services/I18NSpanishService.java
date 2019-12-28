ec3faa30269c452e915d1964e770ee1de66832a8
John Thompson <john@springframework.guru>
2019-12-28 11:46:32 -0500

    adding default profile example

--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/27/19.
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
