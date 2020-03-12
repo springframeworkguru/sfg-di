7576c49a4dc0cc5240ec457be81ab78ab8ef1118
bishal <bishal.git@gmail.com>
2020-03-12 12:07:27 -0500

    t

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,15 +1,23 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgdi.services.PetService;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
-    private final PetService petService;
+	private final PetService petService;
 
-    public String whichPetIsTheBest(){
-        return petService.getPetType();
-    }
+	public PetController(PetService petService) {
+		super();
+		this.petService = petService;
+	}
+
+	public String whichPetIsTheBest() {
+		return petService.getPetType();
+	}
 }
