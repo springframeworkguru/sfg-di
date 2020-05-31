146c441546493759bfe9e1e06e76ad5cc91b2ae0
Kyle R <kyle.retherford@live.com>
2020-05-30 22:16:32 -0500

    init commit

---
+++ src/test/java/net/rethfam/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rethfam.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
