2ee1c7a148bfb8f4cfafd208139280054a9a1cb4
Kyle R <kyle.retherford@live.com>
2020-05-31 22:40:58 -0500

    Default Profile

--- src/main/java/net/rethfam/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/net/rethfam/sfgdi/services/I18nSpanishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by kr on 5/31/2020.
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
