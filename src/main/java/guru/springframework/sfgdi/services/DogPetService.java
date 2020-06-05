47ebf8f29d80581450bda00b98f44e1780b65a7d
BharatVTiwary <56096991+BharatVTiwary@users.noreply.github.com>
2020-06-04 21:59:08 -0400

    completed assignment

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,14 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
-public class DogPetService {
+@Service
+public class DogPetService implements PetService{
     public String getPetType(){
         return "Dogs are the best!";
     }
