dea5c9337cc4a0289343b120c5cb4c4778acba0e
glenn.chatterton@atlassian.com <glenn.chatterton@atlassian.com>
2020-08-30 04:58:19 +1000

    Refactored Unit tests to to follow AAA - Added unit tests for GreetingsController for sayBye and SayWhatsUp

--- src/test/java/guru/springframework/sfgdi/controllers/GreetingControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/GreetingControllerTest.java
@@ -1,23 +1,67 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.ByeGreetingService;
+import guru.springframework.sfgdi.services.GreetingService;
 import guru.springframework.sfgdi.services.HelloGreetingService;
+import guru.springframework.sfgdi.services.WhatUpGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 class GreetingControllerTest {
 
-    private GreetingController greetingController;
-
     @BeforeEach
     void setUp() {
-        greetingController = new GreetingController(new HelloGreetingService());
     }
 
     @Test
-    void sayHello_returns_hello_text() {
+    void sayHello_returns_a_string_with_a_length_greater_than_zero() {
+
+        //arrange
+        GreetingController greetingController = setUpGreetingController(new HelloGreetingService());
+
+        //act
         String result = greetingController.sayHello();
+
+        //assert
         System.out.println(result);
-        Assert.hasText(result,"Hello!");
+        Assert.isTrue(result.length() > 0);
+    }
+
+    @Test
+    void sayBye_returns_a_string_with_a_length_greater_than_zero() {
+
+        //arrange
+        ByeGreetingService byeGreetingService = new ByeGreetingService();
+        GreetingController greetingController = setUpGreetingController(byeGreetingService);
+        greetingController.byeGreetingService = byeGreetingService;
+
+        //act
+        String result = greetingController.sayBye();
+
+        //assert
+        System.out.println(result);
+        Assert.isTrue(result.length() > 0);
+    }
+
+    @Test
+    void sayWhatsUp_returns_a_string_with_a_length_greater_than_zero() {
+
+        //arrange
+        WhatUpGreetingService whatUpGreetingService = new WhatUpGreetingService();
+        GreetingController greetingController = setUpGreetingController(whatUpGreetingService);
+        greetingController.setGreetingServiceSetter(whatUpGreetingService);
+
+        //act
+        String result = greetingController.sayWhatsUp();
+
+        //assert
+        System.out.println(result);
+        Assert.isTrue(result.length() > 0);
+    }
+
+
+    private GreetingController setUpGreetingController(GreetingService greetingService) {
+        return new GreetingController(greetingService);
     }
 }
