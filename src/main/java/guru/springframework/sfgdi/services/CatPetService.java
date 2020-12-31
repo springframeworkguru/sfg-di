3133fd9c8423fac95012f19b455eaf680f7c08b8
DAVID ALONSO TORREJON <DAVID.ALONSO.TORREJON@ing.com>
2020-12-31 14:30:33 +0100

    fixing DI

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,11 +1,20 @@
 package guru.springframework.sfgdi.services;
 
+<<<<<<< Updated upstream
+=======
+import org.springframework.context.annotation.Profile;
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+<<<<<<< Updated upstream
 @Service("cat")
+=======
+@Profile("cat")
+@Service
+>>>>>>> Stashed changes
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+<<<<<<< Updated upstream
+=======
+import org.springframework.stereotype.Service;
+>>>>>>> Stashed changes
 
 /**
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
+<<<<<<< Updated upstream
 public class DogPetService {
+=======
+@Service
+public class DogPetService implements PetService {
+>>>>>>> Stashed changes
     public String getPetType(){
         return "Dogs are the best!";
     }
