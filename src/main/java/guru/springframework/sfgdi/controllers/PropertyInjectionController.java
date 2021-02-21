0ad523a16556ced408631666982bff5401aec0bc
Erno Kajcsa <kajcsaerno@gmail.com>
2021-02-21 12:31:16 +0100

    Spring

---
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectionController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class PropertyInjectionController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgdi.services;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
