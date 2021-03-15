1aec5da61cbc3c8c113dedeee18a74500162b666
pedroshaw <p_shaw@btconnect.com>
2021-03-15 09:43:30 +0000

    Changes

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"cat", "default"})
+@Profile({"cat"})
 @Service
 public class CatPetService implements PetService {
     @Override
