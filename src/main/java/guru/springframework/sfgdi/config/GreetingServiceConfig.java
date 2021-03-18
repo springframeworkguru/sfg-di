09ba65f10e5bd3b58abf3be38808151bf01b27b2
pedroshaw <p_shaw@btconnect.com>
2021-03-18 17:06:38 +0000

    Java Configuration

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * User: p_sha
+ * Date: Thu 18 Mar 2021
+ */
+@Configuration
+public class GreetingServiceConfig {
+
+    // Use @Bean annotation here and remoce the @Service from class...
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return  new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
