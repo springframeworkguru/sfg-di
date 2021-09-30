27711d0998540662d18f5ff854d877862ab7f897
mfedenko <mfedenko@office.atlassian.com>
2021-09-30 22:45:39 +0300

    added spring di

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
