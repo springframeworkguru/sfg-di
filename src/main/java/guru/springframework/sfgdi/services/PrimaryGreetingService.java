64bdb882023d835a9caba80b8a13299baa94f3de
mfedenko <mfedenko@office.atlassian.com>
2021-10-06 14:21:01 +0300

    configured beans with java and added primary and profiles to those beans

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,10 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Primary
-@Service
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
