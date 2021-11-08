ff454b113e9c3078861e539ea0147f8a2415ec4d
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-08 23:11:39 +0900

    1-34-dependency-injection-without-spring

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgdi.services;
+
+public class GreetingServiceImpl
+    implements GreetingService {
+  @Override
+  public String sayGreeting() {
+    return "Hello World";
+  }
+}
