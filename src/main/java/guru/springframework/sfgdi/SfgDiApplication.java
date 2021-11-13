986978b0bfcb7463fc1cd92faf8a8bb199f94d9a
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-13 22:15:19 +0900

    1-36-primary-bean

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -16,9 +16,8 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
+		System.out.println("----------- Primary Been");
+		System.out.println(myController.sayHello());
 
 		System.out.println("----------- Propeerty");
 
--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-  public String sayHello() {
-    System.out.println("Hello $10,000,000,000");
+  private GreetingService greetingService;
 
-    return "Hi Folks!";
+  public MyController(GreetingService greetingService) {
+    this.greetingService = greetingService;
+  }
+
+  public String sayHello() {
+
+    return greetingService.sayGreeting();
   }
 }
