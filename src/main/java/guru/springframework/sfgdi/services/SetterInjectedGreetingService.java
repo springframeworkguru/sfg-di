a96ed86b13d17cfaf1e318848f1b6a554ab0990c
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-20 22:32:59 +0900

    73-java-configuration-exam

--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -1,10 +1,6 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class SetterInjectedGreetingService
-    implements GreetingService {
+public class SetterInjectedGreetingService implements GreetingService {
   @Override
   public String sayGreeting() {
     return "Hello World - setterInjected";
