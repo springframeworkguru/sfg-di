ab52ce10975ecbe9b06283bb83fd42f9cede995c
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-20 22:08:38 +0900

    72-component-scan

--- src/main/java/com/springframework/pets/PetService.java
+++ src/main/java/com/springframework/pets/PetService.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgdi.services;
+package com.springframework.pets;
 
 public interface PetService {
   String getPetType();
--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,9 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
