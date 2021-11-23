cb54629464279e4147bc6b769682449b23e0ae1e
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-23 16:08:51 +0900

    75-di-in-java-configuration

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,6 +1,15 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.inject.Inject;
+
+@RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class I18nEnglishGreetingService implements GreetingService {
+
+  private final @NonNull EnglishGreetingRepository englishGreetingRepository;
 
   @Override
   public String sayGreeting() {
