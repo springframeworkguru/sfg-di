187ba8aef44347c7e8eff430733b248eb99b4160
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-29 22:10:22 +0900

    76-using-factory-bean

--- src/main/java/com/springframework/pets/DogPetService.java
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -1,14 +1,9 @@
 package com.springframework.pets;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile({"dog", "default"})
-@Service
 public class DogPetService implements PetService {
 
   @Override
-  public String getPetType(){
+  public String getPetType() {
     return "Dogs are the best!";
   }
 }
