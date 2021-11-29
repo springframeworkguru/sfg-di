29d68c7993d8b7bd01ef8ebcb2a173121d239638
Lee, Kyungsoo | Will | ECFD <kyungsoo.lee@rakuten.com>
2021-11-29 23:10:30 +0900

    79-spring-bean-scope-example

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.services.PrototypeBean;
+import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -27,21 +29,36 @@ public class SfgDiApplication {
 
 		System.out.println("----------- Propeerty");
 
-		PropertyInjectedController controller = (PropertyInjectedController)  ctx.getBean("propertyInjectedController");
+		PropertyInjectedController controller = (PropertyInjectedController) ctx.getBean(
+				"propertyInjectedController");
 
 		System.out.println(controller.getGreeting());
 
 		System.out.println("----------- Setter");
 
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean(
+				"setterInjectedController");
 
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("----------- Constructor");
 
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean(
+				"constructorInjectedController");
 
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("------------ Bean Scope");
+
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
