e960a69a69db7c859cc79113e8cad8490caa3fef
vz <vz@covizmo.com>
2022-01-16 12:48:02 +0200

    Assignment 4 solution

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile("dog")
+@Profile({"dog","default"})
 @Service("petChooserService")
 public class DogPetService implements PetService {
     @Override
