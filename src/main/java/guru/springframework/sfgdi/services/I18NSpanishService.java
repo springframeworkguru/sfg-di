13169f586ebe09cc9351ab54576826ee1a78646d
Kearns <aine.kearns@accenture.com>
2022-04-27 14:49:21 +0200

    dependency injection

--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/27/19.
  */
-@Profile({"ES", "default"})
-@Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
     public String sayGreeting() {
