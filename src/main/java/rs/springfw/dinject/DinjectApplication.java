44d503553a1a3dbe78a4be9113bc19711aa38203
Rahul.Sharma <Rahul.Sharma@RSHARMA-DEL.ncs.dom>
2022-05-29 19:37:48 +0530

    Primary Bean

--- src/main/java/rs/springfw/dinject/DinjectApplication.java
+++ src/main/java/rs/springfw/dinject/DinjectApplication.java
@@ -18,9 +18,8 @@ public class DinjectApplication {
 		
 		MyController myController = (MyController)ctx.getBean("myController");
 		
-		String greeting = myController.sayHello();
-		
-		System.out.println(greeting);
+		System.out.println("----- Primary Bean");
+		System.out.println(myController.sayHello());
 		
 		System.out.println("-----Property");
 		
--- src/main/java/rs/springfw/dinject/controllers/MyController.java
+++ src/main/java/rs/springfw/dinject/controllers/MyController.java
@@ -2,11 +2,18 @@ package rs.springfw.dinject.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import rs.springfw.dinject.services.GreetingService;
+
 @Controller
 public class MyController {
 	
+	private final GreetingService greetingService;
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
 	public String sayHello() {
-		System.out.println("Hello World !!");
-		return "Hi folks!";
+		return greetingService.sayGreeting();
 	}
 }
