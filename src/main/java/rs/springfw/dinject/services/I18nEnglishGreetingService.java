3edd1b7cdd3b2f802c4654c4292b9b2ff95d9944
Rahul.Sharma <Rahul.Sharma@RSHARMA-DEL.ncs.dom>
2022-05-30 14:31:12 +0530

    Spring default profile

--- src/main/java/rs/springfw/dinject/services/I18nEnglishGreetingService.java
+++ src/main/java/rs/springfw/dinject/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package rs.springfw.dinject.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
 
