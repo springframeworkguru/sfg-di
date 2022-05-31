972ae0654456d10fd6292e825fd886b64fe6dff2
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-05-31 22:36:04 +0800

    Assignment for Dependenct Injection

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,11 +1,10 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 12/28/19.
- */
-@Service("cat")
+@Profile("cat")
+@Service
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
