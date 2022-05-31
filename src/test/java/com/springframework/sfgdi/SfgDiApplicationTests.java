5b2399b943bc711a18443e41256291a1ed400b2c
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-05-31 18:29:25 +0800

    Initial commit

---
+++ src/test/java/com/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
