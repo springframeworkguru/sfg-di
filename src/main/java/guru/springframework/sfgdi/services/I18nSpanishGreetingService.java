affbabc1bf480509de9a9fe1d135d8ff2f9991af
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-05-31 22:13:38 +0800

    Spring profiles

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
