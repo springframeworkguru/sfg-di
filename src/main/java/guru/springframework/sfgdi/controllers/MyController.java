a7be19b1d1f309bb8d65d6334e34e83a0f47a4a9
Sandro Ulsperger <sandro.ulsperger@porscheinformatik.com>
2022-06-14 09:57:53 +0200

    Dependency Injection Different Methods

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgdi.controllers;
-
 import org.springframework.stereotype.Controller;
+import guru.springframework.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController
 {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService)
+    {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
         System.out.println("Hello World!!!");
 
--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgdi.services;
 
-public class PrimaryGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting()
+    {
+        return "Hello World - From the PRIMARY Bean";
+    }
 }
