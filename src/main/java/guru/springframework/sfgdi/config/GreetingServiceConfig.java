36dfd4d4bbe2fbf05263c0d3b64d5903cdfb5c00
Sandro Ulsperger <sandro.ulsperger@porscheinformatik.com>
2022-06-22 10:56:54 +0200

    DI in Java Configuration

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,57 @@
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService (EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+}
