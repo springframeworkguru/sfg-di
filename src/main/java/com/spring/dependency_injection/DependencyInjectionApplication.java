3df5c58edb9a7dc6c68075bb8c0ca27049aa4bd1
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-24 16:55:30 +0200

    Added spring project

---
+++ src/main/java/com/spring/dependency_injection/DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dependency_injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionApplication.class, args);
+	}
+
+}
