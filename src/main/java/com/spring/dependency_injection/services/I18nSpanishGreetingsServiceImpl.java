0b9fe6a4221f5ce098efa6878f48acb516e225c9
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-04 13:54:41 +0200

    Lecture 39 - Default Profile

--- src/main/java/com/spring/dependency_injection/services/I18nSpanishGreetingsServiceImpl.java
+++ src/main/java/com/spring/dependency_injection/services/I18nSpanishGreetingsServiceImpl.java
@@ -3,7 +3,7 @@ package com.spring.dependency_injection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingsServiceImpl implements GreetingsService {
 
