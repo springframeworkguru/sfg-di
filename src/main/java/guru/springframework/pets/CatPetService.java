5377a91723931c9a1128727f68c8caa8742d470e
mnsrt <40939467+mnsrt@users.noreply.github.com>
2022-08-03 10:21:13 +0300

    Implemented component scan

--- src/main/java/guru/springframework/pets/CatPetService.java
+++ src/main/java/guru/springframework/pets/CatPetService.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgdi.services;
+package guru.springframework.pets;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.PetService;
-import org.springframework.context.annotation.Bean;
+import guru.springframework.pets.PetService;
 import org.springframework.stereotype.Controller;
 
 /**
