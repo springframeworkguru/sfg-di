42e7de8cbbccd472e9cdfc1d68d524691769bd72
Vladimir Tsiparsky <vladits@gmail.com>
2022-08-12 23:29:52 +0300

    DI - fixed assignment

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -11,7 +12,7 @@ public class PetController {
 
     private final PetService petService;
 
-    public PetController(PetService petService) {
+    public PetController(@Qualifier("petService") PetService petService) {
         this.petService = petService;
     }
 
