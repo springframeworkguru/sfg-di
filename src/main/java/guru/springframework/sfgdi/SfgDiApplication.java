2dc84acfe03187284268cf60870d96e83305475b
Vladimir Tsiparsky <vladits@gmail.com>
2022-08-12 14:09:40 +0300

    Revert "DI - Primary Bean"
    This reverts commit 33903e6c82a2b7e006d2182e99db26d821ecbd51.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectionController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +14,6 @@ public class SfgDiApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
-
-        System.out.println("------------ Profiler");
-        I18nController i18nController = (I18nController) context.getBean("i18nController");
-        System.out.println(i18nController.sayHello());
 
         MyController myController = (MyController) context.getBean("myController");
 
