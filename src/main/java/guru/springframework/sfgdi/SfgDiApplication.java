db97e15d96b8542cf5bb3022c6f45f27bdc02c0e
Vladimir Tsiparsky <vladits@gmail.com>
2022-08-12 14:25:07 +0300

    DI - Spring Profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectionController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,31 +12,25 @@ public class SfgDiApplication {
 
         ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
 
-        MyController myController = (MyController) context.getBean("myController");
+        System.out.println("----------- Profile");
+        I18nController i18nController = (I18nController) context.getBean("i18nController");
+        System.out.println(i18nController.SayHello());
 
         System.out.println("------------ Primary Bean");
-
+        MyController myController = (MyController) context.getBean("myController");
         System.out.println(myController.sayHello());
 
         System.out.println("------------ Property ");
-
         PropertyInjectionController propertyInjectionController = (PropertyInjectionController) context.getBean("propertyInjectionController");
-
         System.out.println(propertyInjectionController.getGreeting());
 
         System.out.println("------------ Setter ");
-
         SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
-
         System.out.println(setterInjectedController.getGreeting());
 
         System.out.println("------------ Constructor ");
-
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController");
-
         System.out.println(constructorInjectedController.getGreeting());
-
-
     }
 
 }
