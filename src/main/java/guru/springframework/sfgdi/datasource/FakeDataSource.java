85d2c369c16b43980e58ae9116fd370245d9cd2b
efeleke <eyu2.wf@gmail.com>
2022-08-23 22:34:52 -0500

    external property file added for fake data source

---
+++ src/main/java/guru/springframework/sfgdi/datasource/FakeDataSource.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgdi.datasource;
+
+public class FakeDataSource {
+    public String username;
+    public String password;
+    public String jdbcURL;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getJdbcURL() {
+        return jdbcURL;
+    }
+
+    public void setJdbcURL(String jdbcURL) {
+        this.jdbcURL = jdbcURL;
+    }
+}
