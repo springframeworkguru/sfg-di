3277c55b0e7847ce1b0849248368cc21fb58825d
efeleke <eyu2.wf@gmail.com>
2022-08-23 23:16:38 -0500

    added dev and qa env profiles

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.*;
 /**
  * Created by jt on 2/20/21.
  */
-@PropertySource("classpath:datasource.properties")
+//@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
