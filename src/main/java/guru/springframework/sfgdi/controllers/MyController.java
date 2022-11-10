1d55cba209d41abbb1898cb266a70971d1e2cd5b
Neil Goldsmith <neilgoldsmith1218@gmail.com>
2022-11-09 20:37:23 -0500

    created controller

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World!!!");
+
+        return "hi folks";
+    }
 }
