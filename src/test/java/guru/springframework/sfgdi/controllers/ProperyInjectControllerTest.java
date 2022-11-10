ea50abab9d530da4a5f07b739675019c9d64ffc3
Neil Goldsmith <neilgoldsmith1218@gmail.com>
2022-11-09 20:37:46 -0500

    created controller

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ProperyInjectControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProperyInjectControllerTest {
+
+    ProperyInjectController controller;
+
+    @BeforeEach
+    void setUp(){
+        controller = new ProperyInjectController();
+        controller.greetingService = new GreetingServiceImpl();
+    }
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
