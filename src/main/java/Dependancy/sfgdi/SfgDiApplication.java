2989b7b6713827a72845bbd5e101cb3300189ad3
Ahmed laazouzi <laaz.braid@hotmail.fr>
2023-01-11 15:11:35 -0500

    second commit dependancy injection

---
+++ src/main/java/Dependancy/sfgdi/SfgDiApplication.java
@@ -0,0 +1,13 @@
+package Dependancy.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
