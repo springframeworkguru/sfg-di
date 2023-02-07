1ed8c179185f6f2b1105f23e57e72030cd78e692
Karina Sofia Medina <sofi.k.medina@gmail.com>
2023-02-06 23:21:20 -0300

    di fix
    Signed-off-by: Karina Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,15 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
-public class DogPetService {
+@Service("petService")
+public class DogPetService implements PetService{
+    @Override
     public String getPetType(){
         return "Dogs are the best!";
     }
