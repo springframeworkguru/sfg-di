8f3d41347d0811d5c9a3a057b89aa2e1c59714d4
KiraTeresa <kira.soba@gmx.de>
2023-04-12 21:57:40 +0200

    Correcting instances of greeting services in test, according to the DIs

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
