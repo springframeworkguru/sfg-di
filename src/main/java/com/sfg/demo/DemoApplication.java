e9ab32166241ff7b62427b58ed05097c0d4dc4a6
Onkar Agnihotri <onkaragnihotri9@gmail.com>
2023-05-18 15:37:29 +0530

    Implemented the dependency injection

--- src/main/java/com/sfg/demo/DemoApplication.java
+++ src/main/java/com/sfg/demo/DemoApplication.java
@@ -1,14 +1,19 @@
 package com.sfg.demo;
 
+import com.sfg.demo.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Working code base");
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String msg = myController.sayHello();
+
+        System.out.println(msg);
 	}
 
 }
