3b724efe366935a9f8d8a6309bc4417e1101d92f
Onkar Agnihotri <onkaragnihotri9@gmail.com>
2023-05-18 15:30:49 +0530

    Initial Commit

---
+++ src/main/java/com/sfg/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.sfg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Working code base");
+	}
+
+}
