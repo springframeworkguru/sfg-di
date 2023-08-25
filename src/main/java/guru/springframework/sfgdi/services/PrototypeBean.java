f7b79a3d11f930cba15167f02326b8e1eb5a9512
Sofia Medina <sofi.k.medina@gmail.com>
2023-08-24 23:04:13 -0300

    scope prototype added
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/guru/springframework/sfgdi/services/PrototypeBean.java
+++ src/main/java/guru/springframework/sfgdi/services/PrototypeBean.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PrototypeBean
 {
     public PrototypeBean(){
