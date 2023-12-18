6e0bea1ed7093335daf5d881aa9aac0be4bfe792
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-18 23:33:39 +0000

    Dependency Injection Assignment
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(@Qualifier("petService") PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
